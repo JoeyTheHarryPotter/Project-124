cd58526dc0580a9d5758de8bb5666161a840cdf3
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-06-20 16:33:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(150, 150)
+
+    canvas = createCanvas(550, 550);
+    canvas.position(1300, 150);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
